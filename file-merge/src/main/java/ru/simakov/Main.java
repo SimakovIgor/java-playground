bdc1bd2f417e4701e15975d86944e25647797d9d
Igor Simakov <i.simakov@banki.ru>
2023-12-12 10:55:34 +0800

    1. Пример конкатенации двух файлов 2. Лекция по контрактном тестированию

---
+++ file-merge/src/main/java/ru/simakov/Main.java
@@ -0,0 +1,48 @@
+package ru.simakov;
+
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+
+@SuppressWarnings("PMD.AssignmentInOperand")
+public class Main {
+    @SneakyThrows
+    public static void main(final String[] args) {
+        Path newFilePath = Paths.get("PrideAndPrejudiceTranslate.txt");
+        if (Files.exists(newFilePath)) {
+            Files.delete(newFilePath);
+            Files.createFile(newFilePath);
+        } else {
+            Files.createFile(newFilePath);
+        }
+
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+        try (InputStream inputStreamRu = classloader.getResourceAsStream("Pride and Prejudice.txt");
+             InputStreamReader streamReaderRu = new InputStreamReader(Objects.requireNonNull(inputStreamRu), StandardCharsets.UTF_8);
+             BufferedReader readerRu = new BufferedReader(streamReaderRu);
+             InputStream inputStreamEn = classloader.getResourceAsStream("Pride and Prejudice RU.txt");
+             InputStreamReader streamReaderEn = new InputStreamReader(Objects.requireNonNull(inputStreamEn), StandardCharsets.UTF_8);
+             BufferedReader readerEn = new BufferedReader(streamReaderEn)) {
+
+            String lineEn;
+            String lineRu;
+
+            while ((lineEn = readerEn.readLine()) != null && (lineRu = readerRu.readLine()) != null) {
+                String string = lineEn.isEmpty() || lineEn.charAt(0) == '0' || lineEn.charAt(0) == '1'
+                                ? lineEn + System.lineSeparator()
+                                : lineEn + " " + lineRu + System.lineSeparator();
+
+                Files.writeString(newFilePath, string, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+            }
+        }
+    }
+}
